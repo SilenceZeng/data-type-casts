378a384bf2161ee81237cf87e909b2fe4ab5b8b1
angus <tsang1205@qq.com>
2022-04-05 10:15:06 +0800

    fix: cast data type

--- src/main/java/com/github/hcsp/datatype/Cast.java
+++ src/main/java/com/github/hcsp/datatype/Cast.java
@@ -40,12 +40,12 @@ public class Cast {
     // Cast an Long to int
     // 将Long类型转换成int
     public static int Long2int(Long l) {
-        return Math.toIntExact(l);
+        return l.intValue();
     }
 
     // Cast an Double to long
     // 将Double类型转换成long
     public static long double2Long(Double d) {
-        return Math.round(d);
+        return d.longValue();
     }
 }
